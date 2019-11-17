57041e6127857ccf105128be80378c1077834cda
shanshan1629 <2298332885@qq.com>
2019-11-18 00:14:08 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:He-Shanshan/blog.github.io.git master
+git push -f git@github.com:He-Shanshan/vuepress-blog.github.io.git master
 
 cd -
